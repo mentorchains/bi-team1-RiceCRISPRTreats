1ac62933703b3f7a190eff9707e81edf8f090ac9
KellyZhangL <83798182+KellyZhangL@users.noreply.github.com>
2021-06-27 00:06:17 -0400

    committing changes

--- Heatmaps.R
+++ Heatmaps.R
@@ -1,3 +1,8 @@
+# Module 3 Deliverables - Heat Map
+# Kelly Zhang
+# Leila Li
+
+# Download the following packages
 library("affy")
 library("affyPLM")
 library("simpleaffy")
@@ -11,20 +16,12 @@ gse <- ReadAffy(celfile.path = "GSE19804_RAW/")
 df <- as.data.frame(exprs(gse))
 
 # Normalize data
-mas5 <- mas5(gse)
-saveRDS(mas5, "./QC/data/norm/mas5.rds")
-par(mai=c(3,5,1,1,1))
-boxplot(log2(exprs(mas5)), main="Boxplot mas5 Data", ylab="Probe Intensities", las=2, col=c("red","orange","yellow","green","blue","purple"))
-
 rma <- rma(gse)
+rma_exprs <- exprs(rma)
 saveRDS(rma, "./QC/data/norm/rma.rds")
-par(mai=c(3,5,1,1,1))
+par(main=c(3,5,1,1,1))
 boxplot(exprs(rma), main="Boxplot rma Data", ylab="Probe Intensities", las=2, col=c("red","orange","yellow","green","blue","purple"))
 
-gcrma <- gcrma(gse)
-saveRDS(gcrma, "./QC/data/norm/gcrma.rds")
-par(mai=c(3,5,1,1,1))
-boxplot(exprs(gcrma), main="Boxplot gcrma Data", ylab="Probe Intensities", las=2, col=c("red","orange","yellow","green","blue","purple"))
-
 # Create a heatmap with normalized and raw data
-pheatmap <- pheatmap(cor(df))
+corrltn <- cor(df)
+pheatmap(corrltn, annotation_row=df, annotation_col=df)
